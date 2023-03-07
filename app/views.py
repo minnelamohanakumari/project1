1238508672fca14fba88207e303a94bb065c867f
minnelamohanakumari <minnelamohanakumari18gmail.com>
2023-03-07 19:18:10 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def mohana(request):
+    return HttpResponse('iam mohana')
+def sample(request):
+    return HttpResponse('function name is sample')
+def abc(request):
+    return HttpResponse('function name is abc')
