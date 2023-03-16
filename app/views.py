b1ed70477dfe33c48ebaa23bfe01a310b8dd1722
minnelamohanakumari <minnelamohanakumari18gmail.com>
2023-03-16 11:53:58 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def mohana(request):
-    return HttpResponse('iam mohana')
+    return HttpResponse('my name is mohana')
 def sample(request):
     return HttpResponse('function name is sample')
 def abc(request):
